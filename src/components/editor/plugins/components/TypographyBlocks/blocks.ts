064ce27670b65fdbc2f188b923e20c84a3d58a47
Egomessss <ed.gomes97@gmail.com>
2024-03-01 00:55:26 +0100

    traits can change heading and semantic elements

--- src/components/editor/plugins/components/TypographyBlocks/blocks.ts
+++ src/components/editor/plugins/components/TypographyBlocks/blocks.ts
@@ -35,7 +35,7 @@ export default function(editor: Editor, opts: Required<PluginOptions>) {
     toAdd('heading-one') &&
     bm.add('heading-one', {
         ...commonBlockProps,
-        label: 'Heading One',
+        label: 'Heading',
         media: `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-h-1" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19 18v-8l-2 2" /><path d="M4 6v12" /><path d="M12 6v12" /><path d="M11 18h2" /><path d="M3 18h2" /><path d="M4 12h8" /><path d="M3 6h2" /><path d="M11 6h2" /></svg>`,
         content: { type: 'heading-one' },
 
