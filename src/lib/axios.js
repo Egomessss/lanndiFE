117f334029226abef4fc5962991d6e9b6ea28050
Egomessss <ed.gomes97@gmail.com>
2024-04-06 00:14:52 +0200

    fix axios v6 and cors

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -3,12 +3,12 @@ import Axios from 'axios';
 const axios = Axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
   headers: {
-    // 'X-Requested-With': 'XMLHttpRequest',
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': process.env.NEXT_PUBLIC_BACKEND_URL, // replace this your actual origin
-    'Access-Control-Allow-Methods': 'GET,DELETE,PATCH,POST,PUT',
-    'Referer': process.env.NEXT_PUBLIC_BACKEND_URL,
+    'X-Requested-With': 'XMLHttpRequest',
+    // 'Accept': 'application/json',
+    // 'Content-Type': 'application/json',
+    // 'Access-Control-Allow-Origin': process.env.NEXT_PUBLIC_BACKEND_URL, // replace this your actual origin
+    // 'Access-Control-Allow-Methods': 'GET,DELETE,PATCH,POST,PUT',
+    // 'Referer': process.env.NEXT_PUBLIC_BACKEND_URL,
   },
   withCredentials: true,
   withXSRFToken: true,
