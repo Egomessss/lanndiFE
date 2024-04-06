bbc1707788a7fd79cdd11fa3820c0d58c3383b3b
Egomessss <ed.gomes97@gmail.com>
2024-04-06 18:10:49 +0200

    fixxxeeeddddddd v2

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'images.pexels.com',
-                port: '',
-            },
-            {
-                protocol: 'https',
-                hostname: 'assets.lanndi.com',
-                port: '',
-            },
-        ],
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'assets.lanndi.com',
+        port: '',
+      },
+    ],
+  },
+  rewrites: () => {
+    return [
+      {
+        source: '/backend/:path*',
+        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
