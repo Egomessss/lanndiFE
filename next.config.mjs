d40c79dc3750a7ceedf5bf9ab16b9a9651ac4f49
Egomessss <ed.gomes97@gmail.com>
2024-04-06 02:21:45 +0200

    trying proxy for api routes

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'images.pexels.com',
-                port: '',
-            },
-            {
-                protocol: 'https',
-                hostname: 'assets.lanndi.com',
-                port: '',
-            },
-        ],
-    }
-};
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'images.pexels.com',
+          port: '',
+        },
+        {
+          protocol: 'https',
+          hostname: 'assets.lanndi.com',
+          port: '',
+        },
+      ],
+    },
+
+    rewrites: () => {
+      return [
+        {
+          source: '/backend/:path*',
+          destination:`${process.env.NEXT_PUBLIC_BACKEND_URL}'/:path*'`,
+        },
+      ];
+    },
+  }
+;
 
 export default nextConfig;
--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,7 +2,7 @@
 import Axios from 'axios';
 
 const axios = Axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  // baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
   headers: {
     'X-Requested-With': 'XMLHttpRequest',
     'Accept': 'application/json',
