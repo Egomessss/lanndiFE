789d554447563ebe94ba347379a55b9e656ac9cc
Egomessss <ed.gomes97@gmail.com>
2024-04-06 04:20:44 +0200

    Revert "sign up flow fix v1 maybe?"
    This reverts commit a3a2da56f99db0e4fee05a3f4e8e2697c018c278.

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link';
 
-import { FormEventHandler, useState } from 'react';
+import { FormEventHandler } from 'react';
 import { useRouter } from 'next/navigation';
 
 import { Anchor, Button, Checkbox, Divider, PasswordInput, TextInput } from '@mantine/core';
@@ -21,110 +21,85 @@ interface FormErrors {
 
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [shouldRemember, setShouldRemember] = useState(false);
-  const [errors, setErrors] = useState([]);
-  const [status, setStatus] = useState(null);
 
-  // const form = useForm({
-  //   initialValues: {
-  //     email: '',
-  //     password: '',
-  //     shouldRemember: '',
-  //   },
-  // });
 
-  const { login } = useAuth({
-    middleware: 'guest',
-    redirectIfAuthenticated: '/dashboard',
+  const router = useRouter();
+  const { csrf } = useAuth();
+
+  const form = useForm({
+    initialValues: {
+      email: '',
+      password: '',
+      shouldRemember: '',
+    },
   });
 
+  const { mutate: login, isPending } = useMutation({
+      mutationFn: async () => {
+        // Assuming crsf() is an async function that sets up CSRF tokens
+        await csrf();
+        // Now, make your Axios POST request
+        await axios.post('/login', form.values);
 
-  const submitForm = async (event: any) => {
-    event.preventDefault();
-
-    login({
-      email,
-      password,
-      remember: shouldRemember,
-      setErrors,
-      setStatus,
-    });
-  };
+      },
+      onSuccess:
+        () => {
+          const twoWeeksInSeconds = 60 * 60 * 24 * 14;
+          // On successful login, set a cookie to last for 2 weeks
+          document.cookie = `isLoggedIn=true; path=/; max-age=${twoWeeksInSeconds}; secure; samesite=Strict`;
+          // router.push('/');
+        }
+      ,
+      onError:
+        (error) => {
+          console.log('error', error);
+          // @ts-ignore
+          if (error.response.status === 422) {
+            // Handle Laravel validation errors
+            // @ts-ignore
+            form.setErrors(error.response.data.errors || {});
+          } else {
+            notifications.show({
+              title: 'Error',
+              message: error.toString(),
+              // message: 'Something went wrong... Please try again!',
+              color: 'red',
+            });
+          }
+        },
+    },
+  );
 
   const validateBeforeSubmit: FormEventHandler = (e) => {
     e.preventDefault();
     login();
   };
 
-
-  // const { mutate: login, isPending } = useMutation({
-  //     mutationFn: async () => {
-  //       // Assuming crsf() is an async function that sets up CSRF tokens
-  //       await csrf();
-  //       // Now, make your Axios POST request
-  //       await axios.post('/login', form.values);
-  //
-  //     },
-  //     onSuccess:
-  //       () => {
-  //         const twoWeeksInSeconds = 60 * 60 * 24 * 14;
-  //         // On successful login, set a cookie to last for 2 weeks
-  //         document.cookie = `isLoggedIn=true; path=/; max-age=${twoWeeksInSeconds}; secure; samesite=Strict`;
-  //         // router.push('/');
-  //       }
-  //     ,
-  //     onError:
-  //       (error) => {
-  //         console.log('error', error);
-  //         // @ts-ignore
-  //         if (error.response.status === 422) {
-  //           // Handle Laravel validation errors
-  //           // @ts-ignore
-  //           form.setErrors(error.response.data.errors || {});
-  //         } else {
-  //           notifications.show({
-  //             title: 'Error',
-  //             message: error.toString(),
-  //             // message: 'Something went wrong... Please try again!',
-  //             color: 'red',
-  //           });
-  //         }
-  //       },
-  //   },
-  // );
-  //
-  // const validateBeforeSubmit: FormEventHandler = (e) => {
-  //   e.preventDefault();
-  //   login();
-  // };
-
   return (
     <>
-      <form onSubmit={submitForm} className="flex flex-col gap-4 min-w-96">
+      <form onSubmit={validateBeforeSubmit} className="flex flex-col gap-4 min-w-96">
+        {/*<SocialButtons/>*/}
+        {/*<Divider  label="or" labelPosition="center"/>*/}
         {/* Email Address */}
         <TextInput
           type="email"
           leftSection={<IconAt size="1rem" />}
           label="Email"
-          value={email}
-          onChange={event => setEmail(event.target.value)}
-          // error={errors?.email}
+          {...form.getInputProps('email')}
         />
         {/* Password */}
-
         <PasswordInput
           label="Password"
-          value={password}
-          onChange={event => setPassword(event.target.value)}
-          // error={errors?.password}
+          {...form.getInputProps('password')}
         />
+        <Anchor component={Link} size="sm" href="/login">
+          Already registered?
+        </Anchor>
+
         {/* Remember Me */}
         <Checkbox
           label="Remember me"
-          checked={shouldRemember}
-          onChange={(event) => setShouldRemember(event.currentTarget.checked)}
+          {...form.getInputProps('shouldRemember')}
         />
 
         <div className="flex items-center justify-between ">
@@ -135,9 +110,7 @@ const Login = () => {
             Register here
           </Anchor>
 
-          <Button
-            // loading={isLoading}
-            type="submit">Login</Button>
+          <Button loading={isPending} type="submit">Login</Button>
         </div>
       </form>
     </>
--- src/hooks/auth.js
+++ src/hooks/auth.js
@@ -1,87 +1,15 @@
-import useSWR from 'swr';
+'use client'
+
 import axios from '@/lib/axios';
-import { useEffect, useState } from 'react';
-import { useParams, usePathname, useRouter } from 'next/navigation';
-import { useMutation, useQuery } from '@tanstack/react-query';
-import { notifications } from '@mantine/notifications';
+import { useParams, useRouter } from 'next/navigation';
 
 
 export const useAuth = ({ middleware, redirectIfAuthenticated } = {}) => {
   const router = useRouter();
   const params = useParams();
-  const slug = usePathname();
-
-  const isDemo = slug === '/demo';
-
-  // const { data: user, error, mutate } = useSWR('/api/user', () =>
-  const { data: user, error, mutate } = useSWR(isDemo ? null : '/api/user', () =>
-    axios
-      .get('/api/user')
-      .then(res => res.data)
-      .catch(error => {
-        if (error.response.status !== 409) throw error;
-        router.push('/verify-email');
-      }),
-  );
-
-  // const { data: user, mutate, error } = useMutation({
-  //     mutationFn: async () => axios.get('/api/user')
-  //       .then(res => res.data)
-  //       .catch(error => {
-  //         if (error.response.status !== 409) throw error;
-  //         router.push('/verify-email');
-  //       }),
-  //   },
-  // );
 
 
-  const [isLoading, setIsLoading] = useState(false);
-  // Add similar state variables for other actions...
-
-
-  const csrf = () => axios.get('/sanctum/csrf-cookie');
-
-  const register = async ({ setErrors, ...props }) => {
-    setIsLoading(true);
-    await csrf();
-
-    setErrors([]);
-
-    axios
-      .post('/register', props)
-      .then(() => {
-        mutate();
-        setIsLoading(false);
-      })
-      .catch(error => {
-        if (error.response.status !== 422) throw error;
-        setErrors(error.response.data.errors);
-        setIsLoading(false);
-      });
-  };
-
-  const login = async ({ setErrors, setStatus, ...props }) => {
-    await csrf();
-
-    setErrors([]);
-    setStatus(null);
-    setIsLoading(true);
-
-    axios
-      .post('/login', props)
-      .then(() => {
-        mutate();
-        const twoWeeksInSeconds = 60 * 60 * 24 * 14;
-        // On successful login, set a cookie to last for 2 weeks
-        document.cookie = `isLoggedIn=true; path=/; max-age=${twoWeeksInSeconds}; secure; samesite=Strict`;
-        router.push('/');
-      })
-      .catch(error => {
-        if (error.response.status !== 422) throw error;
-        setErrors(error.response.data.errors);
-        setIsLoading(false);
-      });
-  };
+  const csrf = () => axios.get('/sanctum/csrf-cookie')
 
   const forgotPassword = async ({ setErrors, setStatus, email }) => {
     await csrf();
@@ -123,26 +51,12 @@ export const useAuth = ({ middleware, redirectIfAuthenticated } = {}) => {
       .then(response => setStatus(response.data.status));
   };
 
-  const logout = async () => {
-    if (!error) {
-      await axios.post('/logout').then(() => {
-        document.cookie = 'isLoggedIn=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; secure; samesite=Strict';
-        mutate();
-      });
-    }
-
-    window.location.pathname = '/login';
-  };
 
 
   return {
-    isLoading,
-    user,
-    register,
-    login,
+    csrf,
     forgotPassword,
     resetPassword,
     resendEmailVerification,
-    logout,
   };
 };
