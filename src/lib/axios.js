c7a5206f6ee1234c59f4e91ab07cd876a1c39bf6
Egomessss <ed.gomes97@gmail.com>
2024-04-07 10:17:40 +0200

    testing env v2

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,19 +1,14 @@
-import Axios from 'axios';
+import Axios from 'axios'
 
 const axios = Axios.create({
-  // if production localhost: 'http://localhost:8000' else https://api.lanndi.com
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
-  // baseURL: process.env.NODE_ENV === 'production' ? 'https://api.lanndi.com' : process.env.NEXT_PUBLIC_BACKEND_URL,
   headers: {
-    'X-Requested-With': 'XMLHttpRequest',
+    // 'X-Requested-With': 'XMLHttpRequest',
     'Accept': 'application/json',
     'Content-Type': 'application/json',
-    // 'Access-Control-Allow-Origin': process.env.NEXT_PUBLIC_BACKEND_URL, // replace this your actual origin
-    // 'Access-Control-Allow-Methods': 'GET,DELETE,PATCH,POST,PUT',
-    // 'Referer': process.env.NEXT_PUBLIC_BACKEND_URL,
   },
   withCredentials: true,
-  withXSRFToken: true,
-});
+  withXSRFToken: true
+})
 
-export default axios;
+export default axios
