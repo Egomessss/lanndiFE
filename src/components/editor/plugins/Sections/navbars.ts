ae7f66cbd791e725e026166b8761c946509c6141
Egomessss <ed.gomes97@gmail.com>
2024-07-10 16:55:47 +0200

    fixed navbar v4

--- src/components/editor/plugins/Sections/navbars.ts
+++ src/components/editor/plugins/Sections/navbars.ts
@@ -12,16 +12,18 @@ const navbars = (editor: Editor) => {
     const hamburger = document.querySelector(".hamburger-btn");
     const navMenu = document.querySelector(".nav-menu");
 
+    hamburger?.addEventListener("click", mobileMenu);
+    hamburger?.addEventListener("ontouchstart", mobileMenu);
+
     function mobileMenu() {
       hamburger?.classList.toggle("active");
       navMenu?.classList.toggle("active");
     }
 
-    hamburger?.addEventListener("click", mobileMenu);
-
     const navLink = document.querySelectorAll(".nav-link");
 
     navLink.forEach(n => n.addEventListener("click", closeMenu));
+    navLink.forEach(n => n.addEventListener("ontouchstart", closeMenu));
 
     function closeMenu() {
       hamburger?.classList.remove("active");
